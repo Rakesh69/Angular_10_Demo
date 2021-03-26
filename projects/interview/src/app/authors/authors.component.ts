f7bc9bd09b1a29f984570c4bdf953cefe7fb705a
Rakesh <rakeshkaklotar007@gmail.com>
2021-03-26 18:02:33 +0530

    initial setup

---
+++ projects/interview/src/app/authors/authors.component.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020 DS Co., dba dsco - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AuthorSearchRequest } from '@requests/author-search.request';
+import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthorSearchAction } from './store/authors.actions';
+import { selectAllAuthors, selectAuthorsSearching, selectAuthorsSearchStr } from './store/authors.selectors';
+
+@Component({
+  selector: 'ds-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AuthorsComponent implements OnInit {
+  searchStr$ = this.store.select(selectAuthorsSearchStr);
+  searching$ = this.store.select(selectAuthorsSearching);
+
+  authors$ = this.store.select(selectAllAuthors);
+  numAuthors$ = this.authors$.pipe(map(authors => authors.length));
+
+  noAuthorsFound$ = combineLatest(this.searchStr$, this.searching$, this.numAuthors$).pipe(
+    map(([searchStr, searching, numAuthors]) => {
+      return !!searchStr && !searching && !numAuthors;
+    })
+  );
+
+  constructor(private store: Store<any>) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  search(text: string): void {
+    this.store.dispatch(new AuthorSearchAction(new AuthorSearchRequest({query: text})));
+  }
+}
---
+++ projects/interview/src/app/authors/authors.module.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2020 DS Co., dba dsco - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { SharedModule } from '../shared/shared.module';
+import { AuthorsComponent } from './authors.component';
+import { AuthorsEffects } from './store/authors.effects';
+import { authorsReducer } from './store/authors.reducer';
+
+const routes: Routes = [
+  {path: '', component: AuthorsComponent, data: {title: 'Authors'}}
+];
+
+@NgModule({
+  declarations: [AuthorsComponent],
+  imports: [
+    SharedModule,
+    EffectsModule.forFeature([AuthorsEffects]),
+    StoreModule.forFeature('authors', authorsReducer),
+    RouterModule.forChild(routes)
+  ]
+})
+export class AuthorsModule {
+}
